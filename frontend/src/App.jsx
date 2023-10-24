b071446a7eac30d6c086a3c6b093804df66a6b5d
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-10-24 21:36:49 +0530

    UI design ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import NearLogo from "./assets/logo-black.svg";
+import EducationalText from "./container/EducationalText";
+import SignInButton from "./components/SignInButton";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='app__container'>
-      <h1>Hello</h1>
+    <div className="app__container">
+      <SignInButton/>
+      <img src={NearLogo} className="near-logo" />
+      <h1>
+        The contract says: <span className="greeting">{"Hello"}</span>
+      </h1>
+      <div className="change_greeting_container">
+        <span>Change Greeting:</span>
+        <div>
+          <input placeholder="Type Here" className="change_greeting_input" />
+          <button className="save_btn">Save</button>
+        </div>
+      </div>
+      <EducationalText/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
